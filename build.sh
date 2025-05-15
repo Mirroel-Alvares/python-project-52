42b9eed1927dc315a8ee36cc26681c9c7c469800
Aleksey Kuznetsov <Mirroel-Alvares@yandex.ru>
2025-05-15 17:20:11 +0500

    Stage 4.1.2 fix workflow test-check, change build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 set -e
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-#export PATH="$HOME/.local/bin:$PATH"
+#source $HOME/.local/bin/env
+export PATH="$HOME/.local/bin:$PATH"
 
 
 make install && make collectstatic && make migrate
